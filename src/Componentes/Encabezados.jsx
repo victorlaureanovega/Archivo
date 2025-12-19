d08e10a7f7694fd9876d3d6df4cea689bcbc2fc1
victorlaureanovega <victorlaureanovega@gmail.com>
2025-12-19 14:53:03 -0600

    Ajustes en hovers

--- src/Componentes/Encabezados.jsx
+++ src/Componentes/Encabezados.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom';
+import "../CSS/materialize.css";
 
 export default function Encabezado() {
     return (
@@ -12,14 +13,14 @@ export function EncabezadoArchivo() {
     const navigate = useNavigate();
 
     return (
-        <div>
-            <header style={{ backgroundColor: "#222", textAlign: "center", paddingTop: 90, paddingBottom: 90 }}>
+        <header>
+            <div style={{ backgroundColor: "#222", textAlign: "center", paddingTop: 90, paddingBottom: 90 }}>
                 <h1 style={{ color: "#EEE" }}> Victor Laureano Vega </h1>
-            </header>
-            <div class="boton-regresar-inicio" style={{ backgroundColor: "#444" }}>
+            </div>
+            <div class="boton-regresar-inicio" style={{  }}>
                 <p onClick={ () => navigate('/') }
                     style={{ color: "#EEE", padding: 10, textAlign: "center", fontSize: "14px", cursor: "pointer" }}> Regresar a inicio </p>
             </div>
-        </div>
+        </header>
     );
 }
